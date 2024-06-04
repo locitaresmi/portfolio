31b7d20aef67d69086aa1468964c0423a3e12ee5
Wening Dyah Locitaresmi <weninglc@gmail.com>
2024-06-04 17:54:45 +0700

    updated Finku Retention portfolio

--- src/pages/Projects/Finku2023Retention.js
+++ src/pages/Projects/Finku2023Retention.js
@@ -81,10 +81,19 @@ const Finku2023Retention = () => {
                                     <SidebarLink to="section1" smooth={true} duration={500} spy={true} exact={true} offset={-110}>Background</SidebarLink>
                                 </SidebarItem>
                                 <SidebarItem>
-                                    <SidebarLink to="section2" smooth={true} duration={500} spy={true} exact={true} offset={-110}>Process and Findings</SidebarLink>
+                                    <SidebarLink to="section2" smooth={true} duration={500} spy={true} exact={true} offset={-110}>1. Research Planning</SidebarLink>
                                 </SidebarItem>
                                 <SidebarItem>
-                                    <SidebarLink to="section3" smooth={true} duration={500} spy={true} exact={true} offset={-110}>Impact and Results</SidebarLink>
+                                    <SidebarLink to="section3" smooth={true} duration={500} spy={true} exact={true} offset={-110}>2. Participants Recruitment</SidebarLink>
+                                </SidebarItem>
+                                <SidebarItem>
+                                    <SidebarLink to="section4" smooth={true} duration={500} spy={true} exact={true} offset={-110}>3. Interviews</SidebarLink>
+                                </SidebarItem>
+                                <SidebarItem>
+                                    <SidebarLink to="section5" smooth={true} duration={500} spy={true} exact={true} offset={-110}>4. Analysis</SidebarLink>
+                                </SidebarItem>
+                                <SidebarItem>
+                                    <SidebarLink to="section6" smooth={true} duration={500} spy={true} exact={true} offset={-110}>6. Shareback and Impact</SidebarLink>
                                 </SidebarItem>
                                 {/* <SidebarItem>
                                     <SidebarLink to="section4" smooth={true} duration={500} spy={true} exact={true} offset={-110}>Key takeaways</SidebarLink>
@@ -94,35 +103,10 @@ const Finku2023Retention = () => {
                         <TextColumn initial='initial' animate='animate' variants={appearUp}>
                             <TextContainer>
                                 <ProjectHeading>Uncovering the Cause of Low Retention in a Personal Financial Management App</ProjectHeading>
-                                {/* <ProjectParagraph>Twing is a mobile application that aims to increase people’s awareness of Computer Vision Syndrome (CVS) and prevent it from happening. It helps people to stay productive by reducing the unnoticed side-effect of CVS. It generates real-time analytics and suggestions of its user’s behaviour while using the phone.</ProjectParagraph>
-                                <ProjectParagraph>Twing was designed when my two friends and I were in a competition called GEMASTIK in 2021. It’s a national IT competition held annually by Indonesia’s Ministry of Education, Culture, Research, and Technology for university students. Our team was selected as the top 20 teams out of more than 700 teams in the UX Design category.</ProjectParagraph> */}
                                 <ProjectSection id='section1'>
                                     <ProjectSubHeading>Background</ProjectSubHeading>
-                                    {/* <ProjectMainPoints>Our team identified the problem of users’ behaviour regarding increased phone usage from surveys and interviews.</ProjectMainPoints> */}
                                     <ProjectParagraph>Finku was facing a significant drop in power user retention after the first week of use. Users who continued to the third and fourth weeks were minimal. These findings indicate a need for deeper insights into user behaviour and expectations.</ProjectParagraph>
                                     <ProjectParagraph>The primary objective was to evaluate if Finku’s features aligned with user behaviour and expectations, and identify areas for improvement to enhance user retention.</ProjectParagraph>
-                                    {/* <ContentImageContainer>
-                                        <ImageZoom
-                                            src={SurveyResult}
-                                            alt="Twing Survey Result"
-                                            zoom={zoom.current}
-                                        />
-                                        <ImageCaption>Twing Survey Result</ImageCaption>
-                                    </ContentImageContainer> */}
-                                    <ProjectMainPoints>How I planned to crack the case within two weeks:</ProjectMainPoints>
-                                    <ProjectList>
-                                        <ProjectListItem><ListSpan>Usage Data Analysis</ListSpan>: Analysed usage data to understand the behaviour patterns of users who churned and focused on what features they used and their frequency of use.</ProjectListItem>
-                                        <ProjectListItem><ListSpan>In-Depth Interviews</ListSpan>: Conducted 30-minute interviews with over 10 users who had churned. Aimed to gain qualitative insights into their experiences and pain points.</ProjectListItem>
-                                        <ProjectListItem><ListSpan>Benchmarking</ListSpan>: Compared Finku’s features and performance with other leading financial tracking apps.</ProjectListItem>
-                                    </ProjectList>
-                                    <ContentImageContainer>
-                                        <ImageZoom
-                                            src={Method}
-                                            alt="Research Methodology"
-                                            zoom={zoom.current}
-                                        />
-                                        <ImageCaption>Research Methodology</ImageCaption>
-                                    </ContentImageContainer>
                                     <ProjectMainPoints>My Role</ProjectMainPoints>
                                     <ProjectParagraph>I took on the role of a sole end-to-end researcher. My stakeholders were one Product Lead, a Product Manager, and a Product Designer.</ProjectParagraph>
                                     <ContentImageContainer>
@@ -140,25 +124,57 @@ const Finku2023Retention = () => {
 
                                 </ProjectSection>
                                 <ProjectSection id='section2'>
-                                    <ProjectSubHeading>Process and Findings</ProjectSubHeading>
-                                    <ProjectMainPoints>I initiated check-up meetings twice every week with the stakeholders throughout the project for transparency.</ProjectMainPoints>
+                                    <ProjectSubHeading>1. Research Planning</ProjectSubHeading>
+                                    <ProjectParagraph>I started an initialisation meeting at the start of the project with stakeholders to dig deeper into the problem and to find out what kind of impact was expected from this research.</ProjectParagraph>
+                                    <ProjectMainPoints>After that, I planned on how I will crack the case within a two-week time constraint:</ProjectMainPoints>
+                                    <ProjectList>
+                                        <ProjectListItem><ListSpan>Usage Data Analysis</ListSpan>: Analyse usage data using Amplitude to understand the behaviour patterns of users who churned and focus on what features they used and their frequency of use.</ProjectListItem>
+                                        <ProjectListItem><ListSpan>In-Depth Interviews</ListSpan>: Conduct 30-minute interviews with 10 users who had churned. Aimed to gain qualitative insights into their experiences and pain points.</ProjectListItem>
+                                        <ProjectListItem><ListSpan>Benchmarking</ListSpan>: Compare Finku’s features and performance with other leading financial tracking apps.</ProjectListItem>
+                                    </ProjectList>
+                                    <ContentImageContainer>
+                                        <ImageZoom
+                                            src={Method}
+                                            alt="Research Methodology"
+                                            zoom={zoom.current}
+                                        />
+                                        <ImageCaption>Research Methodology</ImageCaption>
+                                    </ContentImageContainer>
+                                    <ProjectMainPoints>I scheduled check-up meetings twice every week with the stakeholders throughout the project for transparency.</ProjectMainPoints>
                                     <ProjectParagraph>My objectives were to ensure:</ProjectParagraph>
                                     <ProjectList>
                                         <ProjectListItem>Stakeholders' expectations were aligned with my research</ProjectListItem>
                                         <ProjectListItem>Stakeholders can empathise more with users by hearing directly the raw testimonials given by users in my interviews</ProjectListItem>
                                     </ProjectList>
+                                    </ProjectSection>
+                                    <ProjectSection id='section3'>
+                                    <ProjectSubHeading>2. Participants Recruitment</ProjectSubHeading>
+                                    <ProjectParagraph>I used MoEngage to send out interview invitations to over 100 Finku users who had stopped using the app. Initially, I aimed to interview at least five users per segment to ensure the reliability of the data.</ProjectParagraph> 
+                                    </ProjectSection>
+                                    <ProjectSection id='section4'>
+                                    <ProjectSubHeading>3. Interviews</ProjectSubHeading>
+                                    <ProjectParagraph>The main goals of the interview were to:</ProjectParagraph>
+                                    <ProjectList>
+                                        <ProjectListItem>Understand their background, including demographic info and personal finance management experience</ProjectListItem>
+                                        <ProjectListItem>Understand their expectations when they installed Finku</ProjectListItem>
+                                        <ProjectListItem>Understand their experience when using the features</ProjectListItem>
+                                    </ProjectList>
+                                    <ProjectParagraph>I stopped reaching out to people for interviews after the answers I got were saturated.</ProjectParagraph>
+                                    </ProjectSection>
+                                    <ProjectSection id='section5'>
+                                    <ProjectSubHeading>4. Analysis</ProjectSubHeading>
                                     <ProjectTrivia>
                                         <TriviaParagraph>My analysis revealed that many users' expectations of Finku's manual transaction tracker weren't being met. They hoped for more a seamless way to record their finances in comparison to the existing methods that were available.</TriviaParagraph>
                                     </ProjectTrivia>
                                     <ProjectMainPoints>User Expectations and Behaviour:</ProjectMainPoints>
                                     <ProjectList>
-                                        <ProjectListItem>Users primarily expected to use Finku for its financial tracking features.</ProjectListItem>
-                                        <ProjectListItem>Manual financial tracking was the most frequently used feature, as many users still used cash for transactions.</ProjectListItem>
+                                        <ProjectListItem>Users primarily expected to use Finku for its financial tracking features</ProjectListItem>
+                                        <ProjectListItem>Manual financial tracking was the most frequently used feature, as many users still used cash for transactions</ProjectListItem>
                                     </ProjectList>
                                     <ProjectMainPoints>User Experience:</ProjectMainPoints>
                                     <ProjectList>
-                                        <ProjectListItem>Most users were new to financial management and found Finku’s interface to be non-intimidating and beginner-friendly.</ProjectListItem>
-                                        <ProjectListItem>Users found the manual financial tracking process to be complex and time-consuming, leading to frustration and eventual churn.</ProjectListItem>
+                                        <ProjectListItem>Most users were new to financial management and found Finku’s interface to be non-intimidating and beginner-friendly</ProjectListItem>
+                                        <ProjectListItem>Users found the manual financial tracking process to be complex and time-consuming, leading to frustration and eventual churn</ProjectListItem>
                                     </ProjectList>
                                     <ContentImageContainer>
                                     <ImageZoom
@@ -179,8 +195,8 @@ const Finku2023Retention = () => {
                                         <ImageCaption>Transaction Record's Duration in Comparison to Other Players</ImageCaption>
                                     </ContentImageContainer>
                                 </ProjectSection>
-                                <ProjectSection id='section3'>
-                                    <ProjectSubHeading>Impact and Results</ProjectSubHeading>
+                                <ProjectSection id='section6'>
+                                    <ProjectSubHeading>Shareback and Impact</ProjectSubHeading>
                                     <ProjectMainPoints>I compiled a comprehensive report and discussed the findings with stakeholders, highlighting user pain points and areas for improvement.</ProjectMainPoints>
                                     <ContentImageContainer>
                                         <ImageZoom
